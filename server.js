409396fb830d028b8f91bcdadec11900ed8d8f23
Felipe Erazo <felipe.erazoangarita@gmail.com>
2021-06-04 15:10:15 -0500

    Changes on webpacke

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = 3000;
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, './client/dist')));
 app.use(express.json());
 
 app.listen(port, (error) => {

c4f1e00cd4cedfe7119ead6e372aea1f0c06fbb3
gitInLoserWereGoingShopping <palmer.mikepalmer@gmail.com>
2021-06-08 10:24:56 -0500

    Add Search Bar - skeleton code

--- client/src/components/Questions_Answers/index.jsx
+++ client/src/components/Questions_Answers/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import SearchBar from "material-ui-search-bar";
 
 /*
 Title - Questions & Answers
@@ -17,11 +18,40 @@ Button for "more answered questions"
 Button for "Add a question +"
 */
 
+/********************  search bar section
+<SearchBar
+    value={this.state.value}
+    onChange={(newValue) => this.setState({ value: newValue })}
+    onRequestSearch={() => doSomethingWith(this.state.value)}
+  />
+  
+  SearchBar Properties
+Name	Type	Default	Description
+cancelOnEscape	bool		Whether to clear search on escape
+classes*	object		Override or extend the styles applied to the component.
+className	string	''	Custom top-level class
+closeIcon	node	<ClearIcon style={{ color: grey[500] }} />	Override the close icon.
+disabled	bool	false	Disables text field.
+onCancelSearch	func		Fired when the search is cancelled.
+onChange	func		Fired when the text value changes.
+onRequestSearch	func		Fired when the search icon is clicked.
+placeholder	string	'Search'	Sets placeholder text for the embedded text field.
+searchIcon	node	<SearchIcon style={{ color: grey[500] }} />	Override the search icon.
+style	object	null	Override the inline-styles of the root element.
+value	string	''	The value of the text field.
+  
+**********************/
+
 //props needed?
 const Questions_Answers = () => {
   return (
     <div>
-      <p>Product Questions_Answers</p>
+      <h2>Questions and Answers</h2>
+      <SearchBar
+        placeholder='Search questions and answers...'
+        onChange={(newValue) => 'this.setState({ value: newValue })'}
+        onRequestSearch={() => 'doSomethingWith(this.state.value)'}
+      />
     </div>
   )
 };

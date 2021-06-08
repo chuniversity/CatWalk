2a78bbde673ca6f9e67a5db3452097e610741a42
gitInLoserWereGoingShopping <palmer.mikepalmer@gmail.com>
2021-06-07 20:41:32 -0500

    pseudocode approach / outline overall high-level gameplan for Q&A feature/section

--- client/src/components/Questions_Answers/index.jsx
+++ client/src/components/Questions_Answers/index.jsx
@@ -1,5 +1,22 @@
 import React from 'react';
 
+/*
+Title - Questions & Answers
+Search bar - searches through questions and answers for string input matches
+Question container/component section
+  Needs to show first/top question that will prioritize the top/first question shown in the following order
+    1. question/answer with highest relevancy based on search term
+    2. Then sorted by most helpful
+    3. Bonus idea would be drop down filter for sorting criteria based on relevancy, newest, or most helpful
+  Question "Q: ..." Helpful? Yes (25) | Add Answer
+  Answer Segments "A: ..."
+    Answer tag at time of answering: "by user1234, January 1, 2019 | Helpful? Yes (2) | Report"
+    Potential photos: "as you can see in these photos: [photos previously uploaded displayed here]"
+    Button for "load more answers"
+Button for "more answered questions"
+Button for "Add a question +"
+*/
+
 //props needed?
 const Questions_Answers = () => {
   return (

4a4335df156c53e90f5ad6ca016c349ca5faaa82
gitInLoserWereGoingShopping <palmer.mikepalmer@gmail.com>
2021-06-09 20:40:41 -0500

    Q&A - Add question, Add answer, modal forms

---
+++ client/src/components/Questions_Answers/QuestionForm.jsx
@@ -0,0 +1,97 @@
+import React from 'react';
+import { FormControl, Button, Modal, makeStyles, TextField } from '@material-ui/core';
+
+
+//set default position of modal pop up to be middle of screen
+const getModalStyle = () => {
+  const top = 50;
+  const left = 50;
+  
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`
+  };
+}
+
+//styling for modal pop up
+const useStyles = makeStyles(theme => ({
+  paper: {
+    position: "absolute",
+    width: 300,
+    backgroundColor: "white",
+    padding: 20
+  }
+}));
+
+
+const QuestionForm = () => {
+  const classes = useStyles();
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+  
+  const handleOpen = () => {
+    setOpen(true);
+  };
+  
+  const handleClose = () => {
+    setOpen(false);
+  };
+  // /console.log(EnteredValue.target.value)
+  const body = (
+    <div style={modalStyle} className={classes.paper}>
+      <h2>Ask Your Question</h2>
+      <h4>{`About the ${'[currentProductNameHere]'}`}</h4>
+      <FormControl action="someURL" method="post" >
+        
+        <TextField
+          id="filled-multiline-static"
+          label="Your Question"
+          multiline
+          required
+          rows={4}
+          variant="outlined"
+          inputProps={{maxLength: 60}}
+          onChange={ input => {
+            if (input.target.value.length === 60) {
+              alert('maximum question character limit of 60 reached!')
+            }
+          }}
+          placeholder="60 character maximum"
+        />
+        <br></br>
+        <TextField
+        id="outlined-nickname"
+        label="Your Nickname"
+        variant="outlined"
+        required
+        placeholder="Example: jack543!"
+        />
+        <br></br>
+        <TextField
+        id="outlined-email"
+        label="Your email"
+        variant="outlined"
+        type="email"
+        required
+        placeholder="Example: jack@email.com"
+        />
+        <br></br>
+        <button type="submit">Submit Question</button>
+      </FormControl>
+    </div>
+  );
+  
+  return (
+    <>
+      <button onClick={handleOpen}>
+        ADD A QUESTION +
+      </button>
+      <Modal open={open} onClose={handleClose}>
+        {body}
+      </Modal>
+    </>
+  )
+}
+
+export default QuestionForm;
--- client/src/components/Questions_Answers/index.jsx
+++ client/src/components/Questions_Answers/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import SearchBar from "material-ui-search-bar";
-// import axios from 'axios';
+import axios from 'axios';
+// import access from '../../../../config.js';
+import QuestionForm from './QuestionForm.jsx';
+import AnswerForm from './AnswerForm.jsx';
 
 /*
 Title - Questions & Answers
@@ -102,12 +105,14 @@ This list does not include any reported questions.
 axios.get('/qa/questions')
   .then(questions => {
     this.setState({
-      questions: questions.results
+      questions: questions.results.data
     })
   })
   .catch(err => console.error(err))
 
 ***********/
+
+
 
 
 let questions = [{
@@ -162,83 +167,76 @@ let questions = [{
 
 //props needed?
 const Questions_Answers = () => {
-  // const {questions} = this.state || props;
+  // const {questions, productId} = this.state || props;
+  
+  // componentDidMount() {
+  //   axios
+  //     .get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/products/${this.props.productId}`, {
+  //       headers: {
+  //         'Authorization': access.token
+  //       }
+  //     })
+  //     .then(questions => {
+  //         this.setState({
+  //           questions: questions.results.data
+  //         })
+  //       })
+  //     .catch(err => console.error(err))
+  // }
   
   return (
-    <div>
+    <>
       <h2>Questions and Answers</h2>
+      
       <SearchBar
         placeholder='Search questions and answers...'
         onChange={(newValue) => 'this.setState({ value: newValue })'}
         onRequestSearch={() => 'doSomethingWith(this.state.value)'}
       />
+      
       <div id="questions">
-        {/*
-        List of questions/answers:
-        Needs to show first/top question that will prioritize the top/first question shown in the following order
-          1. question/answer with highest relevancy based on search term
-          2. Then sorted by most helpful
-          3. Bonus idea would be drop down filter for sorting criteria based on relevancy, newest, or most helpful
-        Question "Q: ..." Helpful? Yes (25) | Add Answer
-        Answer Segments "A: ..."
-          Answer tag at time of answering: "by user1234, January 1, 2019 | Helpful? Yes (2) | Report"
-          Potential photos: "as you can see in these photos: [photos previously uploaded displayed here]"
-          Button for "load more answers"
-        */}
-        {questions.map((question) => (
-          /* each question will have following structure:
-          {
-            "question_id": 37,
-            "question_body": "Why is this product cheaper here than other sites?",
-            "question_date": "2018-10-18T00:00:00.000Z",
-            "asker_name": "williamsmith",
-            "question_helpfulness": 4,
-            "reported": false,
-            "answers": {
-              68: {
-                "id": 68,
-                "body": "We are selling it here without any markup from the middleman!",
-                "date": "2018-08-18T00:00:00.000Z",
-                "answerer_name": "Seller",
-                "helpfulness": 4,
-                "photos": []
-                // ...
-              }
-            }
-          }*/
-          <ul key={question.question_id}>
-            <span style={{'fontWeight': 'bold'}}>
-              {`Q: ${question.question_body}`}
-            </span>
-            <br></br>
-            <span style={{'fontStyle': 'italic'}}>
-              {`Helpful?  Yes (${question.question_helpfulness})  |  Add Answer`}
-            </span>
-            
-            <span>
-              {
-              Object.entries(question.answers).map((answer) => (
-                <ul key={answer[0]}>
-                  <span>
-                    {`A: ${answer[1].body}`}
-                  </span>
-                  <br></br>
-                  <span style={{'fontStyle': 'italic'}}>
-                    {`by ${answer[1].answerer_name}, ${new Date(answer[1].date).toDateString()}  |  Helpful?  Yes (${answer[1].helpfulness})  |  Report`}
-                  </span>
-                  <br></br>
-                  <span>
-                    {`**photos** ${answer[1].photos}`}
-                  </span>
-                </ul>
-              ))
-              }
-            </span>
-            
-          </ul>
-        ))}
+        {questions.map((question, i) => {
+          if (i < 4) {
+            return (
+            <ul key={question.question_id}>
+              <span style={{'fontWeight': 'bold'}}>
+                {`Q: ${question.question_body}`}
+              </span>
+              <br></br>
+              <span style={{'fontStyle': 'italic'}}>
+                {`Helpful?  Yes (${question.question_helpfulness})  |  `}<AnswerForm/>
+              </span>
+              
+              <span>
+                {Object.entries(question.answers).map((answer, j) => { 
+                  if (j < 2) {
+                    return (
+                    <ul key={answer[0]}>
+                      <span>
+                        {`A: ${answer[1].body}`}
+                      </span>
+                      <br></br>
+                      <span style={{'fontStyle': 'italic'}}>
+                        {`by ${answer[1].answerer_name}, ${new Date(answer[1].date).toDateString()}  |  Helpful?  Yes (${answer[1].helpfulness})  |  Report`}
+                      </span>
+                      <br></br>
+                      <span>
+                        {`**photos** ${answer[1].photos}`}
+                      </span>
+                    </ul>
+                    ) 
+                  }
+                })}
+                {(Object.entries(question.answers).length > 2) ? <button onClick={console.log('click - load more answers\n TODO: when button is pressed, display more answers from list of answers for product')}>LOAD MORE ANSWERS</button> : null}
+              </span>
+            </ul>
+            )
+          }
+        })}
+        {(questions.length > 4) ? <button onClick={console.log('click - load more questions\n TODO: when button is pressed, display more questions from list of questions for product')}>MORE ANSWERED QUESTIONS</button> : null}
+        <QuestionForm />
       </div>
-    </div>
+    </>
   )
 };
 

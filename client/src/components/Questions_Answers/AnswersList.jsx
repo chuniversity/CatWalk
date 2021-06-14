565d4c470e1e09bd1c2c36a5f4aaf2d83b367096
gitInLoserWereGoingShopping <palmer.mikepalmer@gmail.com>
2021-06-14 18:28:06 -0500

    Added handling of photos for answers with photos attached, created separate components for answers and questions lists for DRYer code, and added functionality for loading more answers or questions

---
+++ client/src/components/Questions_Answers/AnswersList.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default class AnswersList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 2
+    }
+    this.moreAnswers = this.moreAnswers.bind(this);
+  }
+
+  moreAnswers () {
+    this.setState({
+      quantity: this.state.quantity + 2
+    })
+  }
+  
+  render() {
+    const {question} = this.props;
+    return (<>
+      {Object.entries(question.answers).map((answer, j) => {
+        if (j < this.state.quantity) {
+          return (
+          <ul key={answer[0]}>
+            <span>
+              {`A: ${answer[1].body}`}
+            </span>
+            <br></br>
+            <span style={{'fontStyle': 'italic'}}>
+              {`by ${answer[1].answerer_name}, ${new Date(answer[1].date).toDateString()}  |  Helpful?  Yes (${answer[1].helpfulness})  |  Report`}
+            </span>
+            <br></br>
+            <span>
+              {answer[1].photos.map((photo, k) => {
+                return <img src={photo} key={k} style={{
+                  'border': '1px solid #ddd',
+                  'border-radius': '4px',
+                  'padding': '5px',
+                  'width': '150px'
+                }}/>
+              })}
+            </span>
+          </ul>
+          )
+        } else if (j === this.state.quantity + 1) {
+          return (<button key={`loadAnswers_${j}`} onClick={this.moreAnswers}>Load more answers</button>)
+        }
+      })}
+    </>)
+  }
+}
---
+++ client/src/components/Questions_Answers/QuestionsList.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import AnswersList from './AnswersList.jsx';
+import AnswerForm from './AnswerForm.jsx';
+
+export default class QuestionsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quantity: 4
+    }
+    this.moreQuestions = this.moreQuestions.bind(this);
+  }
+
+  moreQuestions () {
+    this.setState({
+      quantity: this.state.quantity + 2
+    })
+  }
+  
+  render() {
+    const {questions} = this.props;
+    return (<>
+        {questions.map((question, i) => {
+          if (i < this.state.quantity) {
+            return (
+            <ul key={question.question_id}>
+              <span style={{'fontWeight': 'bold'}}>
+                {`Q: ${question.question_body}`}
+              </span>
+              <br></br>
+              <span style={{'fontStyle': 'italic'}}>
+                {`Helpful?  Yes (${question.question_helpfulness})  |  `}<AnswerForm/>
+              </span>
+              <AnswersList question={question}/>
+            </ul>
+            )
+          } else if (i === this.state.quantity + 1) {
+            return (<button key={`loadQuestions_${question.question_id}`} onClick={this.moreQuestions}>Load more answered questions</button>)
+          }
+        })}
+    </>)
+  }
+}

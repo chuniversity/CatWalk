dfd27867685057ef14270a7b1a44b01eab221f39
gitInLoserWereGoingShopping <palmer.mikepalmer@gmail.com>
2021-06-15 16:51:23 -0500

    add search bar functionality, load more questions/answer modal functionality, thumbnail images for picture uploads on answers to questions

--- client/src/components/Questions_Answers/AnswersList.jsx
+++ client/src/components/Questions_Answers/AnswersList.jsx
@@ -1,20 +1,85 @@
 import React from 'react';
-import { Zoom } from '@material-ui/core';
+import axios from 'axios';
+import access from '../../../../config.js';
 
 export default class AnswersList extends React.Component {
+  isMounted = false;
   constructor(props) {
     super(props);
     this.state = {
-      quantity: 2
+      quantity: 2,
+      voted: false,
+      reported: false
     }
     this.moreAnswers = this.moreAnswers.bind(this);
+    this.voteHelpful = this.voteHelpful.bind(this);
+    this.reportAnswer = this.reportAnswer.bind(this);
   }
+  
   
 
   moreAnswers () {
     this.setState({
       quantity: this.state.quantity + 2
     })
+  }
+  
+  voteHelpful(e) {
+    if(!this.state.voted && this.isMounted) {
+      console.log('helpful vote sent!');
+      let answerId = e.target.value;
+      // send put request to increase helpfulness for a specific answer
+      let config = {
+        method: 'put',
+        url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/qa/answers/${answerId}/helpful`,
+        headers: { 
+          'Content-Type': 'application/json', 
+          'authorization': access.token
+        }
+      };
+      axios(config)
+        .then(() => {
+          if (this.isMounted) {
+            this.setState({
+              voted: true
+            })
+          }
+        })
+        .catch(err => console.error(err))
+    }
+  }
+  
+  reportAnswer(e) {
+    if(!this.state.reported && this.isMounted) {
+      console.log('helpful vote sent!');
+      let answerId = e.target.value;
+      // send put request to report a specific answer
+      let config = {
+        method: 'put',
+        url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/qa/answers/${answerId}/report`,
+        headers: { 
+          'Content-Type': 'application/json', 
+          'authorization': access.token
+        }
+      };
+      axios(config)
+        .then(() => {
+          if (this.isMounted) {
+            this.setState({
+              reported: true
+            })
+          }
+        })
+        .catch(err => console.error(err))
+    }
+  }
+  
+  componentDidMount() {
+    this.isMounted = true;
+  }
+  
+  componentWillUnmount() {
+    this.isMounted = false;
   }
   
   render() {
@@ -29,7 +94,7 @@ export default class AnswersList extends React.Component {
             </span>
             <br></br>
             <span style={{'fontStyle': 'italic'}}>
-              {`by ${answer[1].answerer_name}, ${new Date(answer[1].date).toDateString()}  |  Helpful?  Yes (${answer[1].helpfulness})  |  Report`}
+              {`by ${answer[1].answerer_name}, ${(new Date(answer[1].date).toDateString()).slice(3)}  `}<button onClick={this.voteHelpful} value={answer[0]}>{`Helpful? Yes (${answer[1].helpfulness})`}</button><button onClick={this.reportAnswer} value={answer[0]}>Report</button>
             </span>
             <br></br>
             <span>
@@ -46,6 +111,7 @@ export default class AnswersList extends React.Component {
                 />
               })}
             </span>
+            <br></br>
           </ul>
           )
         } else if (j === this.state.quantity + 1) {
--- client/src/components/Questions_Answers/index.jsx
+++ client/src/components/Questions_Answers/index.jsx
@@ -21,106 +21,6 @@ Button for "more answered questions"
 Button for "Add a question +"
 */
 
-/********************  search bar section
-// import SearchBar from "material-ui-search-bar";
-<SearchBar
-    value={this.state.value}
-    onChange={(newValue) => this.setState({ value: newValue })}
-    onRequestSearch={() => doSomethingWith(this.state.value)}
-  />
-  SearchBar Properties
-Name	Type	Default	Description
-cancelOnEscape	bool		Whether to clear search on escape
-classes*	object		Override or extend the styles applied to the component.
-className	string	''	Custom top-level class
-closeIcon	node	<ClearIcon style={{ color: grey[500] }} />	Override the close icon.
-disabled	bool	false	Disables text field.
-onCancelSearch	func		Fired when the search is cancelled.
-onChange	func		Fired when the text value changes.
-onRequestSearch	func		Fired when the search icon is clicked.
-placeholder	string	'Search'	Sets placeholder text for the embedded text field.
-searchIcon	node	<SearchIcon style={{ color: grey[500] }} />	Override the search icon.
-style	object	null	Override the inline-styles of the root element.
-value	string	''	The value of the text field.
-**********************/
-
-/************
-GET /qa/questions Retrieves a list of questions for a particular product.
-This list does not include any reported questions.
-{
-  "product_id": "5",
-  "results": [{
-        "question_id": 37,
-        "question_body": "Why is this product cheaper here than other sites?",
-        "question_date": "2018-10-18T00:00:00.000Z",
-        "asker_name": "williamsmith",
-        "question_helpfulness": 4,
-        "reported": false,
-        "answers": {
-          68: {
-            "id": 68,
-            "body": "We are selling it here without any markup from the middleman!",
-            "date": "2018-08-18T00:00:00.000Z",
-            "answerer_name": "Seller",
-            "helpfulness": 4,
-            "photos": []
-            // ...
-          }
-        }
-      },
-      {
-        "question_id": 38,
-        "question_body": "How long does it last?",
-        "question_date": "2019-06-28T00:00:00.000Z",
-        "asker_name": "funnygirl",
-        "question_helpfulness": 2,
-        "reported": false,
-        "answers": {
-          70: {
-            "id": 70,
-            "body": "Some of the seams started splitting the first time I wore it!",
-            "date": "2019-11-28T00:00:00.000Z",
-            "answerer_name": "sillyguy",
-            "helpfulness": 6,
-            "photos": [],
-          },
-          78: {
-            "id": 78,
-            "body": "9 lives",
-            "date": "2019-11-12T00:00:00.000Z",
-            "answerer_name": "iluvdogz",
-            "helpfulness": 31,
-            "photos": [],
-          }
-        }
-      },
-      // ...
-  ]
-}
-*******/
-
-/* question list backup - to delete later
-  <div id="questions">
-            {questions.map((question, i) => {
-              if (i < 4) {
-                return (
-                <ul key={question.question_id}>
-                  <span style={{'fontWeight': 'bold'}}>
-                    {`Q: ${question.question_body}`}
-                  </span>
-                  <br></br>
-                  <span style={{'fontStyle': 'italic'}}>
-                    {`Helpful?  Yes (${question.question_helpfulness})  |  `}<AnswerForm/>
-                  </span>
-                  <AnswersList question={question}/>
-                </ul>
-                )
-              }
-            })}
-            {(questions.length > 4) ? <button onClick={console.log('click - load more questions\n TODO: when button is pressed, display more questions from list of questions for product')}>Load more answered questions</button> : null}
-          </div>
-*/
-
 export default class Questions_Answers extends React.Component {
   isMounted = false;
   constructor(props) {
@@ -143,20 +43,15 @@ export default class Questions_Answers extends React.Component {
       .then(questions => {
         if (this.isMounted) {
           this.setState({
-            questions: questions.data.results,
-            displayAnswers: !this.state.displayAnswers
+            questions: questions.data.results
           })
         }
       })
-      
   }
 
   componentDidMount() {
     this.isMounted = true;
     this.getQuestions()
-    this.setState({
-      filtered: this.props.questions
-    })
   }
   
   componentWillUnmount() {
@@ -168,12 +63,6 @@ export default class Questions_Answers extends React.Component {
     return (
       <>
         <h2>Questions and Answers</h2>
-        <input type="text" placeholder="Search..." />
-        {/* <SearchBar
-          placeholder='Search questions and answers...'
-          onChange={(newValue) => 'this.setState({ value: newValue })'}
-          onRequestSearch={() => 'doSomethingWith(this.state.value)'}
-        /> */}
         <QuestionsList questions={questions} />
         <QuestionForm />
       </>

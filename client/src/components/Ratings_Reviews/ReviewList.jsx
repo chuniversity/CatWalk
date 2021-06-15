4b4ccf8c8c64038a9ca9fe4cfd1f3cb9f958608e
Chuniversity <contact@chunis.org>
2021-06-15 11:40:35 -0400

    component styling mostly complete

---
+++ client/src/components/Ratings_Reviews/ReviewList.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReviewTile from './ReviewTile.jsx';
+import dummydata from './dummydata.js';
+import Button from '@material-ui/core/Button';
+import reviewStyles from './reviewStyles.js'
+
+
+
+const ReviewList = () => {
+  console.log()
+  return (
+    <>
+    <div>
+    
+     {dummydata.results.map((item) => (
+       <ReviewTile 
+          key={item.review_id}
+          rating={item.rating}
+          summary={item.summary}
+          recommend={item.recommend}
+          response={item.response}
+          body={item.body}
+          date={item.date}
+          reviewer_name={item.reviewer_name}
+          helpfulness={item.helpfulness}
+          photos={item.photos}
+    
+       />
+    ))}
+    </div>
+    <div className="rlist-buttons"><span className="rlist-buttons-more"><Button variant="outlined">MORE REVIEWS</Button></span> <span className="rlist-buttons-add"><Button variant="outlined">ADD A REVIEW +</Button></span></div>
+
+    </>
+  )
+};
+
+export default ReviewList;
---
+++ client/src/components/Ratings_Reviews/reviewStyles.js
@@ -0,0 +1,142 @@
+import { makeStyles } from '@material-ui/core';
+
+const reviewStyles = makeStyles({
+  rrCont: {
+    marginTop: '60px',
+    marginLeft: '225px',
+  },
+  rrTitleCont: {},
+  rrBoxCont: {
+    display: 'grid',
+    gridTemplateColumns: '24.8% 60%',
+    gridGap: '30px',
+  },
+  rrBoxL: {},
+  rrBoxR: {
+    maxWidth: '700px',
+  },
+  rrBoxLMainRating: {
+    display: 'grid',
+    gridTemplateColumns: '33% 66%',
+    girdGap: '30px',
+  },
+  rrBoxLMainRatingL: {
+    fontSize: '4.0em',
+    fontWeight: '700',
+    opacity: '0.7',
+    marginLeft: '3px',
+  },
+  rrBoxLMainRatingR: {},
+  rrBoxLRecommendPercentage: {
+    fontSize: '.91rem', 
+    marginTop: '20px',
+  },
+  rrBoxLStars: {
+    marginTop: '20px',
+  },
+  starsBox: {
+    display: 'grid',
+    gridTemplateColumns: '25% 70%',
+    gridGap: '0px',
+  },
+  starsBoxImg: {
+    height: '27px',
+    width: '180px',
+  },
+  starsBoxL: {
+    fontSize: '.8em',
+  },
+  starsBoxLText: {
+    textDecoration: 'none',
+    fontSize: '.9em',
+    color: '#242320',
+    '&:link': {
+      color: '#242320',
+    },
+    '&:visited': {
+      color: '#242320',
+    },
+    '&:hover': {
+      color: '#242320',
+    },
+  },
+  reviewCount: {
+    fontWeight: '600',
+    paddingLeft: '5px',
+    fontSize: '1.1rem',
+    opacity: '.8',
+    '& select': {
+      backgroundColor: 'transparent',
+      border: 'none',
+      fontWeight: '600',
+      textDecoration: 'underline',
+      fontSize: '1.1rem',
+    },
+  },
+  // review tile
+  rtileCont: {
+    padding: '20px 0px 10px 0px',
+  },
+  rtileStaruserCont: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  rtileUser: {
+    fontSize: '.75em',
+    opacity: '.6',
+  }, 
+  rtileSummary: {
+    fontSize: '1.2em',
+    fontWeight: '600',
+    opacity: '.8',
+    fontStretch: 'ultra-condensed',
+    whiteSpace: 'nowrap',
+    marginTop: '17px',
+  },
+  rtileBody: {
+    whiteSpace: 'pre-line',
+    /* line-height: 2em; */
+  },
+  rtileBodyText: {
+    marginTop: '10px',
+  },
+  rtileRec: {
+    margin: '20px 0px 10px 0px',
+    '& img': {
+      padding: '0px 7px 0px 0px',
+    },
+  },
+  rtileRes: {
+    backgroundColor: '#ededed',
+    padding: '5px 0px 5px 20px',
+    fontSize: '.9rem',
+    opacity: '.8',
+  },
+  rtileHelpful: {
+    marginTop: '29px',
+    fontSize: '.8rem',
+    opacity: '.8',
+    '&:link': {
+      color: '#242320',
+    },
+    '&:visited': {
+      color: '#242320',
+    },
+    '&:hover': {
+      color: '#242320',
+      textDecoration: 'none',
+    },
+  },
+  rtileYes: {
+    paddingLeft: '5px',
+    paddingRight: '15px',
+  },
+  rtileReport: {
+    paddingLeft: '13px',
+  },
+  rtileLinebreak: {
+    marginTop: '17px',
+  },
+});
+
+export default reviewStyles;

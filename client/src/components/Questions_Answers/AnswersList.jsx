6412dad74c62e25d22a72c1a8eefa97207ff59da
gitInLoserWereGoingShopping <palmer.mikepalmer@gmail.com>
2021-06-15 21:40:55 -0500

    refactored and cleaned up some code

--- client/src/components/Questions_Answers/AnswersList.jsx
+++ client/src/components/Questions_Answers/AnswersList.jsx
@@ -16,8 +16,6 @@ export default class AnswersList extends React.Component {
     this.reportAnswer = this.reportAnswer.bind(this);
   }
 
-
-
   moreAnswers () {
     this.setState({
       quantity: this.state.quantity + 2
@@ -27,7 +25,6 @@ export default class AnswersList extends React.Component {
   voteHelpful(e) {
     if(!this.state.voted && this.isMounted) {
       let answerId = e.target.value;
-      // send put request to increase helpfulness for a specific answer
       let config = {
         method: 'put',
         url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/qa/answers/${answerId}/helpful`,
@@ -51,7 +48,6 @@ export default class AnswersList extends React.Component {
   reportAnswer(e) {
     if(!this.state.reported && this.isMounted) {
       let answerId = e.target.value;
-      // send put request to report a specific answer
       let config = {
         method: 'put',
         url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-atx/qa/answers/${answerId}/report`,
@@ -83,20 +79,24 @@ export default class AnswersList extends React.Component {
   render() {
     const {question} = this.props;
     return (<>
-      {Object.entries(question.answers).map((answer, j) => {
+      {Object.entries(question.answers).map((answerTuple, j) => {
+        let answerId = answerTuple[0];
+        let answer = answerTuple[1];
         if (j < this.state.quantity) {
           return (
-          <ul key={answer[0]}>
+          <ul key={answerId}>
             <span>
-              {`A: ${answer[1].body}`}
+              {`A: ${answer.body}`}
             </span>
             <br></br>
             <span style={{'fontStyle': 'italic'}}>
-              {`by ${answer[1].answerer_name}, ${(new Date(answer[1].date).toDateString()).slice(3)}  `}<button onClick={this.voteHelpful} value={answer[0]}>{`Helpful? Yes (${answer[1].helpfulness})`}</button><button onClick={this.reportAnswer} value={answer[0]}>Report</button>
+              {`by ${answer.answerer_name}, ${(new Date(answer.date).toDateString()).slice(3)}  `}
+              <button onClick={this.voteHelpful} value={answerId}>Helpful? Yes ({answer.helpfulness})</button>
+              <button onClick={this.reportAnswer} value={answerId}>Report</button>
             </span>
             <br></br>
             <span>
-              {answer[1].photos.map((photo, k) => {
+              {answer.photos.map((photo, k) => {
                 return <img
                 src={photo}
                 key={`img_${k}`}
@@ -104,12 +104,12 @@ export default class AnswersList extends React.Component {
                   'border': '1px solid #ddd',
                   'borderRadius': '4px',
                   'padding': '5px',
-                  'height': '200px'
+                  'height': '165px'
                 }}
                 />
               })}
-            </span>
             <br></br>
+            </span>
           </ul>
           )
         } else if (j === this.state.quantity + 1) {
